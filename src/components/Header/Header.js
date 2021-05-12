e2f717d6779acbd8bcb12851dd7ed13c308d5282
Kuba009 <jakub.juras@polskifunduszmultimedialny.pl>
2021-05-12 21:52:55 +0200

    prepared repo after create react app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Kuba just dołit</h1> 
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/layouts/App/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import '../../components/Header/Header'
+
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+  </div>
+  );
+};
+
+
+export default App;
